70dea84633bfe864d4d905bcc157cb3f05078fa5
Laio Andrade <laioand98@gmail.com>
2020-09-17 14:00:49 -0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    "es2020": true,
+    browser: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  plugins: ['prettier'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": [
+      "warn",
+      {extensions: ['.jsx', '.js']}
+    ],
+    "import/prefer-default-export": "off",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+  },
+};
