1bd2bb98ab12773df3d097831a8c79b7833484c7
Laio Andrade <laioand98@gmail.com>
2020-09-17 20:20:56 -0300

    add components imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Nav from './Components/Nav/Nav';
+import Info from './Section/Info/Info';
+import Input from './Components/Input/Input';
 
 function App() {
   return (
     <div className="App">
-      <h1>Teste</h1>
+      <Nav />
+      <Info />
+      <Input />
     </div>
   );
 }
