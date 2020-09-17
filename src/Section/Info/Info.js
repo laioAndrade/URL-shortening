f80d03f901f45d2bb7b0410ef3c45620abb1914d
Laio Andrade <laioand98@gmail.com>
2020-09-17 20:19:47 -0300

    add info component

---
+++ src/Section/Info/Info.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './Info.module.css';
+import img from '../../assets/images/illustration-working.svg';
+
+const info = () => (
+  <section className={classes.Info}>
+    <div className={classes.InfoText}>
+      <h1>More than just shorter links</h1>
+      <p>Build your brand’s recognition and get detailed insights on how your links are performing.</p>
+      <button type="submit">Get Started</button>
+    </div>
+    <div className={classes.Img}>
+      <img src={img} alt="illustration working"/>
+    </div>
+  </section>
+)
+  
+export default info;
